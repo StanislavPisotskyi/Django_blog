a22352cfbc7209a0cf1db170a9d05803b8cc7866
Stan Pisotskyi <spisotskyi@gmail.com>
2020-01-12 20:34:12 +0200

    login/logout

--- blog/urls.py
+++ blog/urls.py
@@ -21,5 +21,7 @@ from users import views as user_views
 urlpatterns = [
     re_path(r'^$', app_views.index, name='index_page'),
     re_path(r'^register$', user_views.register, name='register_page'),
+    re_path(r'^login', user_views.sign_in, name='login_page'),
+    re_path(r'^logout$', user_views.sign_out, name='logout_page'),
     path('admin/', admin.site.urls),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import AuthenticationForm
+from django.http import HttpResponse
 
 
 def register(request):
@@ -17,3 +19,25 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def sign_in(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index_page')
+            else:
+                return redirect('login_page')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'users/login.html', {'form': form})
+
+
+def sign_out(request):
+    logout(request)
+    return redirect('login_page')
