8525a3baa6f69813907556ea63024bf495884d0b
Stan Pisotskyi <spisotskyi@gmail.com>
2020-01-13 21:50:04 +0200

    display list, show single article

--- article/views.py
+++ article/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import ArticleForm
+from .models import Article
 
 
 @login_required()
 def get_list(request):
-    return render(request, 'articles/list.html')
+    articles = Article.objects.all()
+    return render(request, 'articles/list.html', {'articles': articles})
 
 
 @login_required()
@@ -18,3 +20,8 @@ def create(request):
     else:
         form = ArticleForm()
     return render(request, 'articles/create.html', {'form': form})
+
+
+def get_one_by_id(request, article_id):
+    article = Article.objects.get(id=article_id)
+    return render(request, 'articles/one.html', {'article': article})
