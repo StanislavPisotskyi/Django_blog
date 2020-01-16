ba785c89cde40e20bb533c2ce71902401de61ac5
Stan Pisotskyi <spisotskyi@gmail.com>
2020-01-16 22:44:38 +0200

    add comment model and migration

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from article.models import Article
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+class Comment(models.Model):
+    content = models.TextField()
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(default=datetime.now)
